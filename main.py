3bae1571f5695b4b6ff93f8c27b8d97a4d44d90d
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 15:06:53 +0300

    fix error on path of logo  image of the app

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ window.config(padx=50, pady=50, bg=YELLOW)
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45, "bold"))
 title_label.grid(column=1, row=0)
 
-tomato_img = PhotoImage(file="tomato.png")
+tomato_img = PhotoImage(file="logo/tomato.png")
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 canvas.create_image(100, 112, image= tomato_img)
 timer_text = canvas.create_text(102, 130, text="00:00",fill="white", font=(FONT_NAME, 30, "bold"))

33746ab2e5ad7987b0425f056c65fab8c18385ec
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 15:06:52 +0300

    make functionality for start timer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import math
 # ---------------------------- CONSTANTS ------------------------------- #
+
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -12,7 +13,29 @@ LONG_BREAK_MIN = 20
 reps = 0
 timer = None
 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        if reps % 2 == 0:
+            check_mark.config(text="\u2713")
+        else:
+            check_mark.config(text="")
+            
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Popdoro")
 window.config(padx=50, pady=50, bg=YELLOW)
